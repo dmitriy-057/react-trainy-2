6cd748297cc51640792809f7fd41a7fb98cafd6d
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-06-26 00:01:04 +0300

    add checkbox

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,20 @@
 import React, { Component} from "react";
+import shortid from "shortid";
 
 class Form extends Component {
-state = {
-    name: "",
-    surname: "",   
-}
+  state = {
+      name: "",
+      surname: "",
+      experience: "junior",
+  };
+
+loginInputId = shortid.generate();
+nameInputId = shortid.generate();
+
 handleChange = event => {
     console.log('currentTarget name', event.currentTarget.name);
     console.log('currentTarget value', event.currentTarget.value);
+
     const {name, value} = event.currentTarget;
 
     this.setState({
@@ -18,30 +25,70 @@ handleChange = event => {
 handleSubmit = event => {
     event.preventDefault();
     console.log(this.state);
+
     this.props.onSubmit(this.state);
+    this.reset();
 }
 
+reset = () => {
+  this.setState({   
+    name: "",
+    surname: "",
+  })
+}
 render() {
     return (
     <form onSubmit={this.handleSubmit}>
-        <label htmlFor="">
+        <label htmlFor={this.loginInputId}>
           Name 
           <input 
+          id={this.loginInputId}
           name="name"
           type="text" 
           value={this.state.name} 
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+          />
         </label> 
-        <label>
+        <label htmlFor={this.nameInputId}>
           Surname
           <input 
+          id={this.nameInputId}
           name="surname"
           type="text" 
           value={this.state.surname} 
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+          />
         </label>
+
+        <p>Your Level</p>
+
+        <label>
+          <input 
+            type="radio" 
+            name="experience" 
+            value="junior" 
+            onChange={this.handleChange} 
+            checked={this.state.experience === "junior"}/> Junior
+        </label>
+        <label>
+          <input 
+            type="radio" 
+            name="experience" 
+            value="middle" 
+            onChange={this.handleChange} 
+            checked={this.state.experience === "middle"}/> Middle
+        </label>
+        <label>
+            <input 
+            type="radio" 
+            name="experience" 
+            value="senior" 
+            onChange={this.handleChange} 
+            checked={this.state.experience === "senior"}/> Senior
+        </label>
+        <br/>
         <button type="submit">Submit</button>
-    </form>         
+    </form>
     )
 }
 }
